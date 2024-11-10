56deed01853516ddb57b7affce10d252261ad8ba
Agentsj <abhijithsj4@gmail.com>
2024-11-10 20:28:14 +0530

    fixed svg not drawing completly

--- js/DrawGraph.js
+++ js/DrawGraph.js
@@ -10,6 +10,7 @@ export class DrawGraph {
         svg.style.position = "absolute";
         svg.style.top = "0";
         svg.style.left = "0";
+        // svg.style.minHeight="100vh"
         svg.style.pointerEvents = "none";
         DrawGraph.SVGcontainer = svg;
         this.warperElement.appendChild(svg);
